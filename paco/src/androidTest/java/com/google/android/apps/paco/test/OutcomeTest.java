b70b3b4ac6a1c049b0dc3351e0c0636a7d926b70
actuallysarah <sphulton@gmail.com>
2015-08-11 11:04:00 -0400

    BeAVer

---
+++ paco/src/androidTest/java/com/google/android/apps/paco/test/OutcomeTest.java
@@ -0,0 +1,27 @@
+package com.google.android.apps.paco.test;
+import com.google.paco.shared.Outcome;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class OutcomeTest {
+
+  @Test
+  public void testDefaultCtor() {
+    Outcome out = new Outcome(0);
+    assertTrue(out.succeeded());
+    assertNull(out.getErrorMessage());
+  }
+  
+  @Test
+  public void testErrorCtor() {
+    String errorMessage = "failed event";
+    Outcome out = new Outcome(0, errorMessage);
+    assertFalse(out.succeeded());
+    assertEquals(errorMessage, out.getErrorMessage());
+  }
+  
+
+}
